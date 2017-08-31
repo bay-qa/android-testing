fa3ed1701469e8f0d86b377b5d5114376242ab9d
Sergey Odintsov <sergio1tsov@gmail.com>
2017-08-30 21:54:08 -0700

    first test case from lesson 2 added

--- app/src/androidTest/java/io/mattcarroll/androidtesting/EspressoSignInTest.java
+++ app/src/androidTest/java/io/mattcarroll/androidtesting/EspressoSignInTest.java
@@ -1,13 +1,49 @@
 package io.mattcarroll.androidtesting;
 
+import android.support.test.rule.ActivityTestRule;
+
 import io.mattcarroll.androidtesting.login.LoginActivity;
+import io.mattcarroll.androidtesting.signup.SignUpActivity;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.matcher.ViewMatchers.hasErrorText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+
 
 /**
  * Created by sodintsov on 8/30/17.
  */
 
 public class EspressoSignInTest {
-
     @Rule
-    public final ActivityTestRule <LoginActivity> ActivityRule = new ActivityTestRule<>(LoginActivity.class, false, true);
+    public final ActivityTestRule<SignUpActivity> activityRule = new ActivityTestRule<>(SignUpActivity.class, false, true);
+
+
+    @Test
+    public void userSignUpPersonalInfoVerifyRequiredFieldsAreRequired(){
+        onView(withId(R.id.button_next))
+                .perform(scrollTo())
+                .perform(click());
+        onView(withId(R.id.edittext_first_name))
+                .check(matches(hasErrorText("Required.")));
+        onView(withId(R.id.edittext_last_name))
+                .check(matches(hasErrorText("Required.")));
+        onView(withId(R.id.edittext_address_line_1))
+                .check(matches(hasErrorText("Required.")));
+        onView(withId(R.id.edittext_address_line_2))
+                .check(matches(hasErrorText("Required.")));
+        onView(withId(R.id.edittext_account_number))
+                .check(matches(hasErrorText("Required.")));
+        onView(withId(R.id.edittext_address_city))
+                .check(matches(hasErrorText("Required.")));
+        onView(withId(R.id.edittext_address_state))
+                .check(matches(hasErrorText("Required.")));
+        onView(withId(R.id.edittext_address_zip))
+                .check(matches(hasErrorText("Required.")));
+        onView(withId(R.id.edittext_bank_name))
+                .check(matches(hasErrorText("Required.")));
+    }
 }
