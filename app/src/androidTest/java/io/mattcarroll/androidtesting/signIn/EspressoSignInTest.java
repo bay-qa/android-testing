e076501d649e1dd517c2aec49db66dc7b1941964
Igor Kalenichenko <igorkalenichenko@Igors-MacBook-Pro.local>
2017-08-30 21:12:55 -0700

    added sign in test No Linked Account Slip Is Displayed

---
+++ app/src/androidTest/java/io/mattcarroll/androidtesting/signIn/EspressoSignInTest.java
@@ -0,0 +1,36 @@
+package io.mattcarroll.androidtesting.signIn;
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import io.mattcarroll.androidtesting.R;
+import io.mattcarroll.androidtesting.SplashActivity;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * Created by igorkalenichenko on 8/30/17.
+ */
+
+public class EspressoSignInTest {
+
+    @Rule
+    public ActivityTestRule<SplashActivity> mActivityTestRule =
+            new ActivityTestRule<>(SplashActivity.class);
+
+    @Test
+    public void userSignInVerifyNoLinkedAccountSlipIsDisplayed() {
+
+        onView(withId(R.id.edittext_email)).perform(typeText("test@gmail.com"));
+        onView(withId(R.id.edittext_password)).perform(typeText("Password123$"));
+        onView(withId(R.id.button_sign_in)).perform(click());
+        onView(withId(R.id.textview_no_accounts)).check(matches(isDisplayed()));
+    }
+}
