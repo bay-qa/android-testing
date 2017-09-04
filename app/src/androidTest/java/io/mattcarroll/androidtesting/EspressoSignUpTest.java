2feff5e5c9718dc5ce4312861eb857c043c97095
VitalyW <vitzeev1@gmail.com>
2017-09-03 22:33:22 -0500

    Refactoring required test, backButtonWork test

--- app/src/androidTest/java/io/mattcarroll/androidtesting/EspressoSignUpTest.java
+++ app/src/androidTest/java/io/mattcarroll/androidtesting/EspressoSignUpTest.java
@@ -3,6 +3,8 @@ package io.mattcarroll.androidtesting;
 
 import android.content.res.Resources;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.NoActivityResumedException;
+import android.support.test.espresso.action.EspressoKey;
 import android.support.test.rule.ActivityTestRule;
 
 import org.junit.Before;
@@ -11,12 +13,20 @@ import org.junit.Test;
 
 import io.mattcarroll.androidtesting.signup.SignUpActivity;
 
+import static android.support.test.espresso.Espresso.closeSoftKeyboard;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.pressKey;
 import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.hasErrorText;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.view.KeyEvent.KEYCODE_MINUS;
+import static junit.framework.Assert.assertTrue;
 
 public class EspressoSignUpTest {
 
@@ -31,28 +41,80 @@ public class EspressoSignUpTest {
         resources = InstrumentationRegistry.getTargetContext().getResources();
     }
 
+    private static void scrollToAndTapNext() {
+        onView(withId(R.id.button_next)).perform(scrollTo(), click());
+    }
+
+    private static void tapNext() {
+        onView(withId(R.id.button_next)).perform(click());
+    }
+
+    private void checkFieldHasError(int fieldId, int errorId) {
+        onView(withId(fieldId))
+                .check(matches(hasErrorText(resources.getString(errorId))));
+    }
+
+    public static void pressBackOnActivity() {
+        // hide key board
+        closeSoftKeyboard();
+        // press back button
+        pressBack();
+    }
+
+    private static void scrollToAndFill(int fieldId, String textToType) {
+
+        EspressoKey underscore = new EspressoKey.Builder()
+                .withShiftPressed(true)
+                .withKeyCode(KEYCODE_MINUS)
+                .build();
+
+        onView(withId(fieldId))
+                .perform(scrollTo(),
+                        pressKey(underscore),
+                        typeText(textToType));
+    }
+
+    @Test
+    public void userSignUpVerifyBackButtonWorksOnEachPage() {
+        // fill in personal info
+        scrollToAndFill(R.id.edittext_first_name, "TestName");
+        scrollToAndFill(R.id.edittext_last_name, "TestLastName");
+        scrollToAndFill(R.id.edittext_address_line_1, "TestAddress");
+        scrollToAndFill(R.id.edittext_address_city, "TestCity");
+        scrollToAndFill(R.id.edittext_address_state, "TestState");
+        scrollToAndFill(R.id.edittext_address_zip, "12345");
+        scrollToAndTapNext();
+        // select interest
+        onView(withText("Chess")).perform(click());
+
+        // tap next button
+        tapNext();
+
+        // first press back button
+        pressBackOnActivity();
+        onView(withText("Basketball")).check(matches(isDisplayed()));
+
+        //second press of back button
+        pressBackOnActivity();
+        onView(withId(R.id.edittext_first_name)).check(matches(isDisplayed()));
+
+        boolean activityFinish = false;
+        try {
+            pressBackOnActivity();
+        } catch (NoActivityResumedException e) {
+            activityFinish = true;
+        }
+        assertTrue(activityFinish);
+    }
+
     @Test
     public void userSignUpPersonalInfoVerifyRequiredFieldsAreRequired() {
-        onView(withId(R.id.button_next))
-                .perform(scrollTo())
-                .perform(click());
-
-        onView(withId(R.id.edittext_first_name))
-                .check(matches(hasErrorText(resources.getString(R.string.input_error_required))));
-
-        onView(withId(R.id.edittext_last_name))
-                .check(matches(hasErrorText(resources.getString(R.string.input_error_required))));
-
-        onView(withId(R.id.edittext_address_line_1))
-                .check(matches(hasErrorText(resources.getString(R.string.input_error_required))));
-
-        onView(withId(R.id.edittext_address_city))
-                .check(matches(hasErrorText(resources.getString(R.string.input_error_required))));
-
-        onView(withId(R.id.edittext_address_state))
-                .check(matches(hasErrorText(resources.getString(R.string.input_error_required))));
-
-        onView(withId(R.id.edittext_address_zip))
-                .check(matches(hasErrorText(resources.getString(R.string.input_error_required))));
+        scrollToAndTapNext();
+        checkFieldHasError(R.id.edittext_first_name, R.string.input_error_required);
+        checkFieldHasError(R.id.edittext_last_name, R.string.input_error_required);
+        checkFieldHasError(R.id.edittext_address_line_1, R.string.input_error_required);
+        checkFieldHasError(R.id.edittext_address_city, R.string.input_error_required);
+        checkFieldHasError(R.id.edittext_address_state, R.string.input_error_required);
+        checkFieldHasError(R.id.edittext_address_zip, R.string.input_error_required);
     }
 }
