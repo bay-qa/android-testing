8205e53b5083140e86c673c96417b9f5efa2c2e7
VitalyW <vitzeev1@gmail.com>
2017-09-06 22:00:18 -0500

    Home work 3

--- app/src/androidTest/java/io/mattcarroll/androidtesting/EspressoSignUpTest.java
+++ app/src/androidTest/java/io/mattcarroll/androidtesting/EspressoSignUpTest.java
@@ -5,6 +5,7 @@ import android.content.res.Resources;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.NoActivityResumedException;
 import android.support.test.espresso.action.EspressoKey;
+import android.support.test.espresso.action.ViewActions;
 import android.support.test.rule.ActivityTestRule;
 
 import org.junit.Before;
@@ -14,6 +15,7 @@ import org.junit.Test;
 import io.mattcarroll.androidtesting.signup.SignUpActivity;
 
 import static android.support.test.espresso.Espresso.closeSoftKeyboard;
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -23,10 +25,14 @@ import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.hasErrorText;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isNotChecked;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static android.view.KeyEvent.KEYCODE_MINUS;
 import static junit.framework.Assert.assertTrue;
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
 
 public class EspressoSignUpTest {
 
@@ -61,8 +67,43 @@ public class EspressoSignUpTest {
         pressBack();
     }
 
-    private static void scrollToAndFill(int fieldId, String textToType) {
+    private void selectItemByText(String item) {
+        onView(withText(item)).perform(click());
+    }
 
+    private static void scrollToItem(String item) {
+        onData(allOf(is(instanceOf(String.class)), is(item))).perform(scrollTo());
+    }
+
+    private void fillInPersonalInfo(
+            String name, String lastName, String address, String city, String state, String zipCode
+                                        ) {
+        scrollToAndFill(R.id.edittext_first_name, name);
+        scrollToAndFill(R.id.edittext_last_name, lastName);
+        scrollToAndFill(R.id.edittext_address_line_1, address);
+        scrollToAndFill(R.id.edittext_address_city, city);
+        scrollToAndFill(R.id.edittext_address_state, state);
+        scrollToAndFill(R.id.edittext_address_zip, zipCode);
+    }
+
+    public void checkIfItemSelected(String item) {
+        onView(withText(item)).check(matches(isNotChecked()));
+    }
+
+    public void fillInCredentials(String email, String password) {
+        scrollToAndFill(R.id.autocompletetextview_email, email);
+        scrollToAndFill(R.id.edittext_password, password);
+    }
+
+    public static void clickSignUp() {
+        onView(withId(R.id.button_next)).perform(click());
+    }
+
+    public void checkSuccessfulPopUp() {
+        onView(withId(R.id.alertTitle)).check(matches(allOf(withText("Signup successful!"), isDisplayed())));
+    }
+
+    private static void scrollToAndFill(int fieldId, String textToType) {
         EspressoKey underscore = new EspressoKey.Builder()
                 .withShiftPressed(true)
                 .withKeyCode(KEYCODE_MINUS)
@@ -70,19 +111,17 @@ public class EspressoSignUpTest {
 
         onView(withId(fieldId))
                 .perform(scrollTo(),
-                        pressKey(underscore),
+//                        pressKey(underscore),
                         typeText(textToType));
     }
 
     @Test
     public void userSignUpVerifyBackButtonWorksOnEachPage() {
         // fill in personal info
-        scrollToAndFill(R.id.edittext_first_name, "TestName");
-        scrollToAndFill(R.id.edittext_last_name, "TestLastName");
-        scrollToAndFill(R.id.edittext_address_line_1, "TestAddress");
-        scrollToAndFill(R.id.edittext_address_city, "TestCity");
-        scrollToAndFill(R.id.edittext_address_state, "TestState");
-        scrollToAndFill(R.id.edittext_address_zip, "12345");
+        fillInPersonalInfo(
+                "TestName", "TestLastName", "TestAddress", "TestCity", "TestState", "12345"
+        );
+
         scrollToAndTapNext();
         // select interest
         onView(withText("Chess")).perform(click());
@@ -116,5 +155,27 @@ public class EspressoSignUpTest {
         checkFieldHasError(R.id.edittext_address_city, R.string.input_error_required);
         checkFieldHasError(R.id.edittext_address_state, R.string.input_error_required);
         checkFieldHasError(R.id.edittext_address_zip, R.string.input_error_required);
+    }
+
+    @Test
+    public void userSignUpPositiveTest() {
+        fillInPersonalInfo(
+                "Vitaly", "W", "460E 5th Avenue", "New York", "NY", "10012"
+        );
+        scrollToAndTapNext();
+        scrollToItem("Astronomy");
+        selectItemByText("Astronomy");
+        tapNext();
+        pressBackOnActivity();
+        scrollToItem("Astronomy");
+        checkIfItemSelected("Astronomy");
+        selectItemByText("Astronomy");
+        tapNext();
+        //fill in credentials
+        fillInCredentials("a@a.com", "password");
+        //click sign up
+        clickSignUp();
+        // verify the pop up
+//        checkSuccessfulPopUp();
     }
 }
