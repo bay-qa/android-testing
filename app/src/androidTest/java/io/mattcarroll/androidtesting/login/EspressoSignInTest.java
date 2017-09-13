3f0e7645b5d5805970132ecc757437726afeede9
VitalyW <vitzeev1@gmail.com>
2017-09-12 22:41:14 -0500

    HW 4: linkBankAccount test, split in functions, all data is in user prop file

--- app/src/androidTest/java/io/mattcarroll/androidtesting/login/EspressoSignInTest.java
+++ app/src/androidTest/java/io/mattcarroll/androidtesting/login/EspressoSignInTest.java
@@ -7,6 +7,7 @@ import android.support.test.rule.ActivityTestRule;
 import org.junit.Rule;
 import org.junit.Test;
 
+import io.mattcarroll.androidtesting.BaseTest;
 import io.mattcarroll.androidtesting.R;
 import io.mattcarroll.androidtesting.SplashActivity;
 
@@ -17,7 +18,7 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
-public class EspressoSignInTest {
+public class EspressoSignInTest extends BaseTest {
 
     @Rule
     public final ActivityTestRule<SplashActivity> activityRule =
@@ -25,16 +26,49 @@ public class EspressoSignInTest {
 
     @Test
     public void userSignInVerifyNoLinkedAccounts() {
-        onView(ViewMatchers.withId(R.id.edittext_email))
-                .perform(typeText("a@a.com"));
-
-        onView(withId(R.id.edittext_password))
-                .perform(typeText("password"));
-
-        onView(withId(R.id.button_sign_in))
-                .perform(click());
-
+        signIn();
         onView(withId(R.id.textview_no_accounts))
                 .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void linkBankAccount() {
+        signIn();
+        //user click + sign
+        clickAddButton();
+        //user click Link Account
+        linkButtonClik();
+        //user types in Bank name, Account Number, and Password
+        linkNewBankAccount();
+        //user clicks Link
+        linkButtonClik();
+    }
+
+    private void clickAddButton() {
+        onView(withId(R.id.fab_manage_accounts))
+                .perform(click());
+    }
+
+    private void linkButtonClik() {
+        onView(withId(R.id.button_link_account))
+                .perform(click());
+    }
+
+    private void linkNewBankAccount() {
+        onView(withId(R.id.edittext_bank_name))
+                .perform(typeText(getProperties().getProperty("bankName")));
+        onView(withId(R.id.edittext_account_number))
+                .perform(typeText(getProperties().getProperty("accountNumber")));
+        onView(withId(R.id.edittext_password))
+                .perform(typeText(getProperties().getProperty("password")));
+    }
+
+    private void signIn() {
+        onView(ViewMatchers.withId(R.id.edittext_email))
+                .perform(typeText(getProperties().getProperty("email")));
+        onView(withId(R.id.edittext_password))
+                .perform(typeText(getProperties().getProperty("password")));
+        onView(withId(R.id.button_sign_in))
+                .perform(click());
     }
 }
