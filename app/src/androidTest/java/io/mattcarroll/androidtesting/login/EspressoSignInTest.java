5c584ffd054e4d17a1efffc59528f8aa307035f0
okyska <okyska@yahoo.com>
2017-10-16 23:26:10 -0700

    added test logInAndBankAccount

--- app/src/androidTest/java/io/mattcarroll/androidtesting/login/EspressoSignInTest.java
+++ app/src/androidTest/java/io/mattcarroll/androidtesting/login/EspressoSignInTest.java
@@ -3,6 +3,7 @@ package io.mattcarroll.androidtesting.login;
 
 import android.support.test.rule.ActivityTestRule;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -17,6 +18,7 @@ import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 public class EspressoSignInTest extends BaseTest{
 
@@ -30,7 +32,7 @@ public class EspressoSignInTest extends BaseTest{
         onView(withId(R.id.edittext_email))
                 .perform(typeText("email"));
         onView(withId(R.id.edittext_password))
-                .perform(typeText("password"));
+                .perform(typeText("passwordForLogin"));
         onView(withId(R.id.button_sign_in))
                 .perform(
                         scrollTo(),
@@ -38,5 +40,64 @@ public class EspressoSignInTest extends BaseTest{
                 );
         onView(withId(R.id.textview_no_accounts))
                 .check(matches(isDisplayed()));
+    }
+
+    //homework lesson 4
+
+    private static void fillFields(int fieldId, String textToType){
+        onView(withId(fieldId)).perform(
+                typeText(textToType)
+        );
+    }
+
+    private static void scrollToButton(int fieldId){
+        onView(withId(fieldId)).perform(scrollTo());
+
+    }
+
+    private static void clickButton(int fieldId){
+        onView(withId(fieldId)).perform(click());
+    }
+
+    private static void checkTextIsDisplayed(String textToCheck){
+        onView(withText(textToCheck)).check(matches(isDisplayed()));
+    }
+
+    @Before
+    public void SignIn(){
+        //fill email
+        fillFields(R.id.edittext_email, getProperties().getProperty("email"));
+
+        //fill password
+        fillFields(R.id.edittext_password, getProperties().getProperty("passwordForLogin"));
+
+        //scroll to sign in button
+        scrollToButton(R.id.button_sign_in);
+
+        //click on sign in button
+        clickButton(R.id.button_sign_in);
+    }
+
+    @Test
+    public void logInAndBankAccount(){
+
+        //click on + button
+        clickButton(R.id.fab_manage_accounts);
+
+        //click on link button
+        clickButton(R.id.button_link_account);
+
+        //fill bank name
+        fillFields(R.id.edittext_bank_name, getProperties().getProperty("bankName"));
+
+        //fill bank account number
+        fillFields(R.id.edittext_account_number, getProperties().getProperty("accountNumber"));
+
+        //fill password for account
+        fillFields(R.id.edittext_password, getProperties().getProperty("passwordForBankAccount"));
+
+        //click link button
+        clickButton(R.id.button_link_account);
+
     }
 }
